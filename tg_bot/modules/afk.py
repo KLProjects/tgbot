41a6ec371bec2c6a746faf64f3ccdeff82a736fa
Restyled.io <commits@restyled.io>
2021-08-11 14:25:43 +0000

    Restyled by yapf

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -28,9 +28,8 @@ def afk(bot: Bot, update: Update):
         reason = ""
 
     sql.set_afk(update.effective_user.id, reason)
-    update.effective_message.reply_text(
-        "{} is now AFK!".format(update.effective_user.first_name)
-    )
+    update.effective_message.reply_text("{} is now AFK!".format(
+        update.effective_user.first_name))
 
 
 @run_async
@@ -42,17 +41,15 @@ def no_longer_afk(bot: Bot, update: Update):
 
     res = sql.rm_afk(user.id)
     if res:
-        update.effective_message.reply_text(
-            "{} is no longer AFK!".format(update.effective_user.first_name)
-        )
+        update.effective_message.reply_text("{} is no longer AFK!".format(
+            update.effective_user.first_name))
 
 
 @run_async
 def reply_afk(bot: Bot, update: Update):
     message = update.effective_message  # type: Optional[Message]
     entities = message.parse_entities(
-        [MessageEntity.TEXT_MENTION, MessageEntity.MENTION]
-    )
+        [MessageEntity.TEXT_MENTION, MessageEntity.MENTION])
     if message.entities and entities:
         for ent in entities:
             if ent.type == MessageEntity.TEXT_MENTION:
@@ -60,9 +57,8 @@ def reply_afk(bot: Bot, update: Update):
                 fst_name = ent.user.first_name
 
             elif ent.type == MessageEntity.MENTION:
-                user_id = get_user_id(
-                    message.text[ent.offset : ent.offset + ent.length]
-                )
+                user_id = get_user_id(message.text[ent.offset:ent.offset +
+                                                   ent.length])
                 if not user_id:
                     # Should never happen, since for a user to become AFK they must have spoken. Maybe changed username?
                     return
@@ -78,8 +74,7 @@ def reply_afk(bot: Bot, update: Update):
                     res = "{} is AFK!".format(fst_name)
                 else:
                     res = "{} is AFK! says its because of:\n{}".format(
-                        fst_name, user.reason
-                    )
+                        fst_name, user.reason)
                 message.reply_text(res)
 
 
@@ -98,11 +93,12 @@ __mod_name__ = "AFK"
 
 AFK_HANDLER = DisableAbleCommandHandler("afk", afk)
 AFK_REGEX_HANDLER = DisableAbleRegexHandler("(?i)brb", afk, friendly="afk")
-NO_AFK_HANDLER = MessageHandler(
-    Filters.all & Filters.group, no_longer_afk, edited_updates=True
-)
+NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.group,
+                                no_longer_afk,
+                                edited_updates=True)
 AFK_REPLY_HANDLER = MessageHandler(
-    Filters.entity(MessageEntity.MENTION) | Filters.entity(MessageEntity.TEXT_MENTION),
+    Filters.entity(MessageEntity.MENTION)
+    | Filters.entity(MessageEntity.TEXT_MENTION),
     reply_afk,
     edited_updates=True,
 )
